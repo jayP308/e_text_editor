0a24cd57af70ff95b91601d6e212622023f5ae59
Junnel Padilla <junnelpadilla307@gmail.com>
2023-07-21 00:32:13 -0700

    fixed webpack.config.js getting an error even when succesfully connecting to server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,33 +2,75 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-
-module.exports = {
-  entry: "./src/js/index.js",
-  output: {
-    filename: "main.bundle.js",
-    path: __dirname + "/dist",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      // webpack html with favicon
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J. A. T. E.',
+        favicon: './favicon.ico'
+      }),
+      // service worker
+      new GenerateSW({
+        swDest: './src-sw.js'
+      }),
+      // manifest for install
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name: 'jate',
+        description: 'Just Another Text Editor',
+        background_color: '#ff00ff',
+        theme_color: "#ff00ff",
+        orientation: "portrait",
+        display: "standalone",
+        start_url: "./",
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+    // loaders for CSS, JS, and Images
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
           },
         },
-      },
-      // Add the following rule for CSS files
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+        {
+          test: /\.png/,
+          type: 'asset/resource',
+        }
+      ],
+    },
+  };
 };
